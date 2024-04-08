3a0e3dbb2833f2bbd66003491b5d93a3aa35afb9
Levi Duarte <levigduarte5@gmail.com>
2024-04-08 12:29:39 -0700

    new files added: 4/8/24

--- myscript.js
+++ myscript.js
@@ -1,2 +1,31 @@
 const numbers = document.getElementById("numbers");
 
+const projectsPage = document.getElementById("projects-page")
+const aboutPage = document.getElementById("about-page")
+const contactPage = document.getElementById("contact-page");
+
+
+function pageDisplay(display){
+    switch(display){
+        case "projects":
+            projectsPage.style.display = "block";
+            aboutPage.style.display = "none";
+            contactPage.style.display = "none"
+            break;
+        case "about":
+            projectsPage.style.display = "none";
+            aboutPage.style.display = "grid";
+            contactPage.style.display = "none";
+            break;
+        case "contact":
+            projectsPage.style.display = "none";
+            aboutPage.style.display = "none";
+            contactPage.style.display = "block";
+            break;
+    }
+}
+
+numbers.forEach(num => {
+    let ran = Math.floor(Math.random()*2)
+    
+})
